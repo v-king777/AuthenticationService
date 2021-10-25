6555ecc1237407680e62f20d04b5df5530818f6e
v-king777 <v.kolpashchikov@gmail.com>
2021-10-26 00:19:43 +0300

    Добавьте файлы проекта.

---
+++ AuthenticationService/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        
+        public UserController(ILogger logger)
+        {
+            _logger = logger;
+
+            _logger.WriteEvent("Сообщение о событии в программе");
+            _logger.WriteError("Сообщение об ошибке в программе");
+        }
+
+        [HttpGet]
+        public User GetUser()
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                Login = "ostap",
+                Password = "12345678",
+                FirstName = "Остап",
+                LastName = "Бендер",
+                Email = "bender@gmail.com"
+            };
+        }
+    }
+}
---
+++ AuthenticationService/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationService
+{
+    public class Logger : ILogger
+    {
+        public void WriteEvent(string eventMessage)
+        {
+            Console.WriteLine(eventMessage);
+        }
+
+        public void WriteError(string errorMessage)
+        {
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
